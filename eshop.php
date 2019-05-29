96333e8b849903244b986ed558a0eb6b92da26e9
bambodpser <48384002+bambodpser@users.noreply.github.com>
2019-05-29 13:40:27 +0300

    Add php file

---
+++ eshop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>e-Shop || BFMV</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="icon" href="image/bfmv_icon.jpg" />
+</head>
+<body>
+<div class="navi">
+  <ul>
+    <li>Total: €</li>
+    <li>Items:</li>
+    <li class="left"><a class="active" href="index.html">Home</a></li>
+    <li class="left"><a class="active" href="eshop.php">e-Shop</a></li>
+  </ul>
+</div>
+</body>
+</html>
