08093d9d4bc2d1c37f13db2ca758a19bc7a2f519
bambodpser <48384002+bambodpser@users.noreply.github.com>
2019-05-29 13:44:31 +0300

    Connect mySql and Add products

--- eshop.php
+++ eshop.php
@@ -16,5 +16,32 @@
     <li class="left"><a class="active" href="eshop.php">e-Shop</a></li>
   </ul>
 </div>
+<?php
+$connect = mysqli_connect('localhost', 'username', 'password', 'table');
+$query = 'SELECT * FROM products ORDER by id ASC';
+$result = mysqli_query($connect, $query);
+if($result):
+  if(mysqli_num_rows($result)>0):
+    while ($product = mysqli_fetch_assoc($result)):
+      ?>
+      <div class="col-sm-4 col-md-3">
+        <form method="post" action="eshop.php?action=add&id=<?php echo $product['id']; ?>">
+          <div class="products">
+            <img src="<?php echo $product['image'] ?>" class="img-responsive" />
+            <h4 class="text-danger"><?php echo $product['name'] ?></h4>
+            <h4>€ <?php echo $product['price'] ?></h4>
+            <input type="text" name="quantity" class="form-control" value="1" />
+            <input type="hidden" name="image" value="<?php echo $product['image'] ?>" />
+            <input type="hidden" name="name" value="<?php echo $product['name'] ?>" />
+            <input type="hidden" name="price" value="<?php echo $product['price'] ?>" />
+            <input type="submit" name="add_to_cart" class="btn" value="Add to Cart" />
+          </div>
+        </form>
+      </div>
+      <?php
+    endwhile;
+  endif;
+endif;
+?>  
 </body>
 </html>
