9dbb759d5f441cc3fc32080b33a3a5cd5272979d
tsilios <48380999+tsilios@users.noreply.github.com>
2019-05-29 13:53:04 +0300

    Add delete action

--- eshop.php
+++ eshop.php
@@ -42,6 +42,14 @@ else {
   );
 }
 }
+if(filter_input(INPUT_GET, 'action') == 'delete'){
+  foreach ($_SESSION['shopping_cart'] as $key => $product){
+    if($product['id'] == filter_input(INPUT_GET, 'id')){
+      unset($_SESSION['shopping_cart'][$key]);
+    }
+  }
+  $_SESSION['shopping_cart'] = array_values($_SESSION['shopping_cart']);
+}
 $total = 0;
 $totalQ = 0;
 foreach ($_SESSION['shopping_cart'] as $key => $product){
